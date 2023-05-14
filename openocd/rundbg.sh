f7ee68834169a5a3e7abd457a310c997f1e24b22
baldstrom <jadenbottemiller@gmail.com>
2023-05-14 01:03:19 -0700

    Adding rundbg linux script

---
+++ openocd/rundbg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+if [[ $1 == "start" ]]; then
+    echo "Starting stlink server..."
+    openocd -f ${SCRIPTPATH}/stlink.cfg -f ${SCRIPTPATH}/stm32f4x.cfg &>/dev/null & 
+elif [[ $1 == "end" ]]; then
+    echo "Killing stlink server... if avail"
+    pkill -f openocd &>/dev/null &
+else
+    echo "call with 'start' or 'end'"
+fi
--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 int main()
 {
 
-
     for(;;)
     {
         asm volatile("NOP");
