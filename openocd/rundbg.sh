a2bba2be2ed26b5ae5f9ae5239aca55e5091ad6f
baldstrom <jadenbottemiller@gmail.com>
2023-05-14 01:16:23 -0700

    Adding gitignore

--- openocd/rundbg.sh
+++ openocd/rundbg.sh
@@ -9,5 +9,5 @@ elif [[ $1 == "end" ]]; then
     echo "Killing stlink server... if avail"
     pkill -f openocd &>/dev/null &
 else
-    echo "call with 'start' or 'end'"
+    echo "Call with 'start' or 'end'"
 fi
