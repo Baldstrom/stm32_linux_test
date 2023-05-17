e5c5cd1c7a0aa01dd7b00b61d72fa0ed8a6916ae
baldstrom <jadenbottemiller@gmail.com>
2023-05-16 20:09:12 -0700

    dma regs written

--- src/hal/regs/dma_regs.h
+++ src/hal/regs/dma_regs.h
@@ -128,62 +128,123 @@ typedef struct
 
 typedef struct 
 {
-
-} __attribute__((packed)) dma_s0cr_t;
-
-typedef struct 
-{
-
-} __attribute__((packed)) dma_s0ndtr_t;
-
-typedef struct 
-{
-
-} __attribute__((packed)) dma_s0par_t;
-
-typedef struct 
-{
-
-} __attribute__((packed)) dma_s0m0ar_t;
-
-typedef struct 
-{
-
-} __attribute__((packed)) dma_s0m1ar_t;
-
-typedef struct 
-{
-
-} __attribute__((packed)) dma_s0fcr_t;
+    __REG32 en : 1;
+    __REG32 dmeie : 1;
+    __REG32 teie : 1;
+    __REG32 htie : 1;
+    __REG32 tcie : 1;
+    __REG32 pfctrl : 1;
+    __REG32 dir : 2;
+    __REG32 circ : 1;
+    __REG32 pinc : 1;
+    __REG32 minc : 1;
+    __REG32 psize : 2;
+    __REG32 msize : 2;
+    __REG32 pincos : 1;
+    __REG32 pl : 2;
+    __REG32 dbm : 1;
+    __REG32 ct : 1;
+    __REG32 : 1;
+    __REG32 pburst : 3;
+    __REG32 mburst : 3;
+    __REG32 chsel : 3;
+    __REG32 : 4;
+} __attribute__((packed)) dma_sxcr_t;
 
 typedef struct 
 {
-
-} __attribute__((packed)) dma_s1cr_t;
-
-typedef struct 
-{
-
-} __attribute__((packed)) dma_s1ndtr_t;
+    __REG32 ndt : 16;
+    __REG32 : 16;
+} __attribute__((packed)) dma_sxndtr_t;
 
 typedef struct 
 {
-
-} __attribute__((packed)) dma_s1par_t;
-
-typedef struct 
-{
-
-} __attribute__((packed)) dma_s1m0ar_t;
+    __REG32 pa;
+} __attribute__((packed)) dma_sxpar_t;
 
 typedef struct 
 {
-
-} __attribute__((packed)) dma_s1m1ar_t;
+    __REG32 m0a;
+} __attribute__((packed)) dma_sxm0ar_t;
 
 typedef struct 
 {
+    __REG32 m1a;
+} __attribute__((packed)) dma_sxm1ar_t;
 
-} __attribute__((packed)) dma_s1fcr_t;
+typedef struct 
+{
+    __REG32 fth : 2;
+    __REG32 dmdis : 1;
+    __REG32 fs : 3;
+    __REG32 : 1;
+    __REG32 feie : 1;
+    __REG32 : 24;
+} __attribute__((packed)) dma_sxfcr_t;
+\
+typedef struct
+{
+    dma_lisr_t DMA_LISR;
+    dma_hisr_t DMA_HISR;
+    dma_lifcr_t DMA_LIFCR;
+    dma_hifcr_t DMA_HIFCR;
 
-// REPEAT FOR 7 total DMAs
+    dma_sxcr_t DMA_S0CR;
+    dma_sxndtr_t DMA_S0NDTR;
+    dma_sxpar_t DMA_S0PAR;
+    dma_sxm0ar_t DMA_S0M0AR;
+    dma_sxm1ar_t DMA_S0M1AR;
+    dma_sxfcr_t DMA_S0FCR;
+    
+    dma_sxcr_t DMA_S1CR;
+    dma_sxndtr_t DMA_S1NDTR;
+    dma_sxpar_t DMA_S1PAR;
+    dma_sxm0ar_t DMA_S1M0AR;
+    dma_sxm1ar_t DMA_S1M1AR;
+    dma_sxfcr_t DMA_S1FCR;
+
+    dma_sxcr_t DMA_S2CR;
+    dma_sxndtr_t DMA_S2NDTR;
+    dma_sxpar_t DMA_S2PAR;
+    dma_sxm0ar_t DMA_S2M0AR;
+    dma_sxm1ar_t DMA_S2M1AR;
+    dma_sxfcr_t DMA_S2FCR;
+
+    dma_sxcr_t DMA_S3CR;
+    dma_sxndtr_t DMA_S3NDTR;
+    dma_sxpar_t DMA_S3PAR;
+    dma_sxm0ar_t DMA_S3M0AR;
+    dma_sxm1ar_t DMA_S3M1AR;
+    dma_sxfcr_t DMA_S3FCR;
+
+    dma_sxcr_t DMA_S4CR;
+    dma_sxndtr_t DMA_S4NDTR;
+    dma_sxpar_t DMA_S4PAR;
+    dma_sxm0ar_t DMA_S4M0AR;
+    dma_sxm1ar_t DMA_S4M1AR;
+    dma_sxfcr_t DMA_S4FCR;
+
+    dma_sxcr_t DMA_S5CR;
+    dma_sxndtr_t DMA_S5NDTR;
+    dma_sxpar_t DMA_S5PAR;
+    dma_sxm0ar_t DMA_S5M0AR;
+    dma_sxm1ar_t DMA_S5M1AR;
+    dma_sxfcr_t DMA_S5FCR;
+
+    dma_sxcr_t DMA_S6CR;
+    dma_sxndtr_t DMA_S6NDTR;
+    dma_sxpar_t DMA_S6PAR;
+    dma_sxm0ar_t DMA_S6M0AR;
+    dma_sxm1ar_t DMA_S6M1AR;
+    dma_sxfcr_t DMA_S6FCR;
+
+    dma_sxcr_t DMA_S7CR;
+    dma_sxndtr_t DMA_S7NDTR;
+    dma_sxpar_t DMA_S7PAR;
+    dma_sxm0ar_t DMA_S7M0AR;
+    dma_sxm1ar_t DMA_S7M1AR;
+    dma_sxfcr_t DMA_S7FCR;
+} __attribute__((packed)) dma_regs_t;
+
+static dma_regs_t *DMA1 = (dma_regs_t *)(DMA1_BASEADDR);
+static dma_regs_t *DMA2 = (dma_regs_t *)(DMA2_BASEADDR);
